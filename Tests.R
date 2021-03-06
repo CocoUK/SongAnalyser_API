8c15bc0c46ae89428597d5f37f2d9ee813f81277
CocoUK <coco.escobar.palafox@gmail.com>
2021-03-06 21:12:52 +0000

    First commit

---
+++ Tests.R
@@ -0,0 +1,13 @@
+
+library(testthat)
+
+test_that(
+  "Checing for count lyrics",
+  {
+    expect_equal(song_lyrics("queen", "liar")$word_count,316)
+    expect_equal(song_lyrics("queen", "my fairy king")$word_count,230)
+    expect_equal(song_lyrics("kylie Minogue", "spinning around")$word_count,313)
+    expect_equal(song_lyrics("kylie Minogue", "turn it into love")$word_count,313)
+  }
+)
+
