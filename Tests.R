e156298563238b5ffa1e098346442ec22c7ed7d7
CocoUK <coco.escobar.palafox@gmail.com>
2021-03-07 11:57:10 +0000

    Added test for stats

--- Tests.R
+++ Tests.R
@@ -1,5 +1,6 @@
 
 library(testthat)
+library(readxl)
 
 test_that(
   "Checing for count lyrics",
@@ -11,3 +12,18 @@ test_that(
     expect_equal(song_lyrics("Enrique Iglesias", "Bailamos")$word_count,142)
     expect_equal(song_lyrics("Queen", "procession")$word_count,NA)
 })
+
+# Tests artists stats function
+
+### Load test data from file
+test_data <- "C:/Users/me1gae/Documents/SongAnalyser_API/test.xlsx"
+data <- read_excel(path = test_data, sheet = "artist_data")
+count <- read_excel(path = test_data, sheet = "lyric_count")
+df <- read_excel(path = test_data, sheet = "df")
+                          
+
+test_that(
+  "Checing for summary of statistics",
+  {
+    expect_equal(round(artist_stats(data, count)$mean,0),round(df$mean,0))
+  })
