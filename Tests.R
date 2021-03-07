a077eee1be895a6f0a1a3e0439d87a355797c745
CocoUK <coco.escobar.palafox@gmail.com>
2021-03-07 09:51:48 +0000

    Included tests for instrumental and words in brackets

--- Tests.R
+++ Tests.R
@@ -7,8 +7,7 @@ test_that(
     expect_equal(song_lyrics("queen", "liar")$word_count,316)
     expect_equal(song_lyrics("queen", "my fairy king")$word_count,230)
     expect_equal(song_lyrics("kylie Minogue", "spinning around")$word_count,313)
-    expect_equal(song_lyrics("kylie Minogue", "turn it into love")$word_count,149)
+    expect_equal(song_lyrics("kylie Minogue", "turn it into love")$word_count,138)
     expect_equal(song_lyrics("Enrique Iglesias", "Bailamos")$word_count,142)
-  }
-)
-
+    expect_equal(song_lyrics("Queen", "procession")$word_count,NA)
+})
